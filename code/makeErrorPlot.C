a719b6ed31288c1f2f531ff5d806ec423d65fdea
nucleosynthesis <n.c.k.wardle@googlemail.com>
2014-08-07 17:43:39 +0200

    Adding distribution of errors plot from Matt

---
+++ code/makeErrorPlot.C
@@ -0,0 +1,56 @@
+{
+
+  gROOT->ProcessLine(".x paperStyle.C");
+  TFile *fi = TFile::Open("symmetrized_errors_powerlawtoys_compare_corrections.root");
+
+
+  TCanvas *can = new TCanvas();  
+ 
+  err_hist_pow_c0->SetLineColor(2);
+  err_hist_pow_c0->SetLineWidth(2);
+  err_hist_pow_c0->SetMarkerColor(2);
+  err_hist_pow_c0->SetMarkerStyle(20);
+  err_hist_pow_c0->SetMarkerSize(0.8);
+
+  err_hist_pow_c0->GetXaxis()->SetRangeUser(0.4,0.7);
+
+  // Envelope
+  err_hist_env_c1->SetLineWidth(2);
+  err_hist_env_c1->SetLineColor(1);
+  err_hist_env_c1->SetMarkerColor(1);
+  err_hist_env_c1->SetMarkerStyle(23);
+  err_hist_env_c1->SetMarkerSize(1.1);
+
+  err_hist_env_cP->SetLineWidth(2);
+  err_hist_env_cP->SetLineColor(kBlue);
+  err_hist_env_cP->SetMarkerColor(kBlue);
+  err_hist_env_cP->SetMarkerStyle(25);
+  err_hist_env_cP->SetMarkerSize(0.7);
+  
+  err_hist_env_c2->SetLineWidth(2);
+  err_hist_env_c2->SetLineColor(kMagenta);
+  err_hist_env_c2->SetMarkerColor(kMagenta);
+  err_hist_env_c2->SetMarkerStyle(28);
+  err_hist_env_c2->SetMarkerSize(1.1);
+ 
+  err_hist_pow_c0->Draw("pE");
+  err_hist_env_c1->Draw("pEsame");
+  err_hist_env_cP->Draw("pEsame");
+  err_hist_env_c2->Draw("pEsame");
+
+  err_hist_pow_c0->Draw("histsame");
+  err_hist_env_c1->Draw("histsame");
+  err_hist_env_cP->Draw("histsame");
+  err_hist_env_c2->Draw("histsame");
+
+  TLegend *leg = new TLegend(0.52,0.52,0.87,0.89);
+  leg->SetFillColor(0);
+  leg->AddEntry(err_hist_pow_c0,"fit power law","PLE");
+  leg->AddEntry(err_hist_env_c1,"c=1 ","PLE");
+  leg->AddEntry(err_hist_env_cP,"c=p(#chi^{2}) ","PLE");
+  leg->AddEntry(err_hist_env_c2,"c=2 ","PLE");
+  leg->Draw();
+  can->RedrawAxis();
+
+  can->SaveAs("../correction/compare_error_magnitude.pdf");
+}
