1062382256f7f4c140580089cbe9dd7a434bfafc
Matthew <matthew.william.kenzie@cern.ch>
2014-11-17 14:09:04 +0100

    Allows numpy use on lxplus6

--- code/setup_root_lxplus.sh
+++ code/setup_root_lxplus.sh
@@ -23,7 +23,7 @@ if [ "$scAr" = "slc6_amd64_gcc472" ] || [[ $vers == *6.* ]]; then
 	. /afs/cern.ch/sw/lcg/external/gcc/4.7/x86_64-slc6-gcc47-opt/setup.sh
 	. /afs/cern.ch/sw/lcg/app/releases/ROOT/5.34.18/x86_64-slc6-gcc47-opt/root/bin/thisroot.sh
 	export PATH=/afs/cern.ch/sw/lcg/external/Python/2.7.3/x86_64-slc6-gcc47-opt/bin:$PATH
-	export PYTHONPATH=/afs/cern.ch/sw/lcg/external/Python/2.7.3/x86_64-slc6-gcc47-opt/bin:$PWD:$PYTHONPATH
+	export PYTHONPATH=/afs/cern.ch/sw/lcg/external/Python/2.7.3/x86_64-slc6-gcc47-opt/bin:/afs/cern.ch/user/m/mkenzie/my-numpy/INSTALL/lib/python2.7/site-packages:$PWD:$PYTHONPATH
 	export LD_LIBRARY_PATH=/afs/cern.ch/sw/lcg/external/Python/2.7.3/x86_64-slc6-gcc47-opt/lib:$PWD:$LD_LIBRARY_PATH
 	export BOOSTPATH=/afs/cern.ch/sw/lcg/external/Boost/1.53.0_python2.7/x86_64-slc6-gcc47-opt:$BOOSTPATH
 
